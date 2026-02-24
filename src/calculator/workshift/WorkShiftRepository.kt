5ef6a11b6fabecc953b8830dc3362186f5f23725
NoOnesThere <d.zlobenets@gmail.com>
2026-02-25 00:11:47 +0200

    store workshifts

--- src/calculator/workshift/WorkShiftRepository.kt
+++ src/calculator/workshift/WorkShiftRepository.kt
@@ -1,14 +1,7 @@
 package calculator.workshift
 
 import klite.jdbc.BaseCrudRepository
-import klite.jdbc.insert
-import klite.toValues
 import javax.sql.DataSource
 
 
-class WorkShiftRepository(db: DataSource) : BaseCrudRepository<WorkShift, WorkShiftId>(db, "work_shift") {
-
-  fun store(workShift: WorkShift) {
-    db.insert(table, workShift.toValues())
-  }
-}
+class WorkShiftRepository(db: DataSource) : BaseCrudRepository<WorkShift, WorkShiftId>(db, "work_shift")
